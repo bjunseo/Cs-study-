e69e8c5c851aeb43c1cfc08091ee7f860cae9f53
leejunseo <junseo010234@gmail.com>
2020-09-15 19:54:02 +0900

    add communication set

--- controler/controler/MainWindow.xaml.cs
+++ controler/controler/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO.Ports;
+using System.IO;
 
 namespace controler
 {
@@ -21,31 +22,40 @@ namespace controler
     /// </summary>
     public partial class MainWindow : Window
     {
+        private SerialPort _serialPort;
+
         public MainWindow()
         {
             InitializeComponent();
-            PortList.ItemsSource = SerialPort.GetPortNames();
-        }
+            string[] Portnom = SerialPort.GetPortNames();
 
-        private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            ComboBox com = sender as ComboBox;
-            if(com != null)
+            foreach (string portnum in Portnom)
             {
-                ComboBoxItem PORT = com.SelectedItem as ComboBoxItem;
-                if(PORT != null)
-                {
-                    string nowPORT = PORT.Content.ToString();
-                }
+                PortList.Items.Add(portnum);
             }
         }
 
         private void click_startBTN(object sender, RoutedEventArgs e)
         {
-            /*if(!serialPort1.IsOpen)
+            _serialPort = new SerialPort();
+            if(_serialPort.IsOpen)
             {
+                _serialPort.PortName = PortList.DataContext.ToString();
+                _serialPort.BaudRate = (int)baudrate.DataContext;
+                _serialPort.DataBits = (int)8;
+                _serialPort.Parity = Parity.None;
+                _serialPort.StopBits = StopBits.One;
+                _serialPort.ReadTimeout = (int)500;
+                _serialPort.WriteTimeout = (int)500;
+                //_serialPort.DataReceived += new SerialDataReceivedEventHandler(serialPort_DataReceived);
 
-            }*/
+                _serialPort.Open();
+
+                status.Text = "연결되었습니다.";
+                PortList.IsEnabled = false;
+            }
+            else
+                status.Text = "연결되어있습니다.";
         }
     }
 }
